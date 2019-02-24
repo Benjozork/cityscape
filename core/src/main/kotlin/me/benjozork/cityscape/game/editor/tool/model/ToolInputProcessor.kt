5611fc8fe1c2e788a07769e699d2286ccf723ac1
Benjozorrk <benjozorkfr@gmail.com>
2019-02-24 10:37:20 -0500

    removed unnecessary abstraction of InputProcessor

--- core/src/main/kotlin/me/benjozork/cityscape/game/editor/tool/model/ToolInputProcessor.kt
+++ core/src/main/kotlin/me/benjozork/cityscape/game/editor/tool/model/ToolInputProcessor.kt
@@ -1,14 +1,10 @@
 package me.benjozork.cityscape.game.editor.tool.model
 
-import com.badlogic.gdx.InputProcessor
+import com.badlogic.gdx.InputAdapter
 
 import me.benjozork.cityscape.game.editor.tool.EditorTool
-import me.benjozork.cityscape.game.input.AbstractInputProcessor
 
 /**
- * A special implementation of [InputProcessor] that isn't an interface, therefore
- * not requiring to implement all methods, including ones we don't use.
- *
  * @constructor the parent associated [EditorTool]
  */
-open class ToolInputProcessor(val tool: EditorTool) : AbstractInputProcessor()
+open class ToolInputProcessor(val tool: EditorTool) : InputAdapter()
